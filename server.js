a62737123c4af4081d385a576368ec348da3c956
Anniina K <t1kian00@students.oamk.fi>
2022-04-14 13:04:01 +0300

    redefining paths

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/home.html"));
+  res.sendFile(path.join(__dirname, "/backend/home.html"));
 });
 app.use('/', serveStatic(path.join(__dirname, 'Public')));
 
