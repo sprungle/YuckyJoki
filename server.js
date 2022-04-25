8a7f71dbc47d9d015dc3187b1d480f7dbddfc155
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-04-25 14:34:46 +0300

    add database and password and authenication

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 //IMPORTANT: If this is edited, make sure all referrals to other files have correct path!
 
-const serveStatic = require('serve-static')
-const path = require('path');
-const express = require("express");
+
+const serveStatic= require('serve-static');
+const path=require('path');
 const bodyParser= require('body-parser');
+const express = require('express');
 const bcrypt = require('bcrypt');
+
+
 const app = express();
+
+
+
 app.use(express.json());
 
-// connect to the database
+// connect database and get the data from it 
 app.get('/db', async (req, res) => {
   const { Pool } = require('pg');
   const pool = (() => {
@@ -31,32 +37,105 @@ app.get('/db', async (req, res) => {
 }
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/backend/back.home.html"));
-});
-app.use('/', serveStatic(path.join(__dirname, 'backend/public')));
-
-app.get("/home", function(req, res) {
-  res.sendFile(path.join(__dirname, "/backend/back.home.html"));
-});
-app.use('/home', serveStatic(path.join(__dirname, 'backend/public')));
-
-app.get("/about", function(req, res) {
-  res.sendFile(path.join(__dirname, "/backend/back.about.html"));
-});
-app.use('/about', serveStatic(path.join(__dirname, 'backend/public')));
-
-app.get("/contact", function(req, res) {
-    res.sendFile(path.join(__dirname, "/backend/back.contact.html"));
+//home page
+app.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
   });
-app.use('/contact', serveStatic(path.join(__dirname, 'backend/public')));
+app.use('/', serveStatic(path.join(__dirname, 'public')));
 
-app.get("/registrationForm", function(req, res) {
-  res.sendFile(path.join(__dirname, "/backend/back.registrationForm.html"));
+// get the registrationForm
+app.get('/registrationForm', (req, res) => {
+  res.sendFile(path.join(__dirname, 'registrationForm.html'));
 });
-app.use('/registrationForm', serveStatic(path.join(__dirname, 'backend/public')));
+app.use('/registrationForm', serveStatic(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+// post data from the registration to database
+app.post('/registrationForm', async (req, res) => {
+  const { Pool } = require('pg');
+  const pool = (() => {
+  return new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+  rejectUnauthorized: false
+  }
+  });
+  })();
+  try {
+  const {fName, email, phoneNumber,password} = req.body;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  const client = await pool.connect();
+  client.query('INSERT INTO usersInfo VALUES (DEFAULT,$1, $2, $3,$4)',[fName, email, phoneNumber,hashedPassword]);
+  //const results = { 'results': (result) ? result.rows : null};
+  //res.json( results );
+  client.release();
+  } catch (err) {
+  console.error(err);
+  res.json({ error: err });
+}
+});
+// get login form
+app.get('/login', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+  });
+  app.use(bodyParser.urlencoded({extended: false}));
+  // post data from the registration to database
+  
+  app.post('/login', async (req, res) => {
+    const { Pool } = require('pg');
+    const pool = (() => {
+    return new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+    }
+    });
+    })();
+    const {Email, Password} = req.body;
+    const client = await pool.connect();
+    const user = await client.query('SELECT email, password FROM usersInfo WHERE email=$1;',[Email])
+    const loginUser = (user) ? user.rows : null;
+   
+    if (user == null) {
+      return res.status(400).send('Cannot find user')
+    }
+    
+    try {
+  
+      if(await bcrypt.compare(req.body.Password, loginUser[0].password)) {
+        res.send('Logged in successfully');
+      } else {
+        res.send('Not Allowed')
+      }
+    
+    
+    
+    
+    //res.json( results );
+  
+    client.release();
+    } catch (err) {
+    console.error(err);
+    res.json({ error: err });
+  }
+  });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
-});
+
+
+// contact page
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
+  });
+app.use('/contact', serveStatic(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+
+// loscal server
+const PORT =process.env.PORT ||3010;
+app.listen(PORT,()=> {
+  console.log(`listening on ${PORT}......`)
+  });
