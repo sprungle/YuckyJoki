fac62f6cbc6dd80d34f49034c832aedd749abd18
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-04-26 07:51:51 +0300

    delete credentials

--- database.sql
+++ database.sql
@@ -1,12 +1,4 @@
--- heroku database credentials
---Connection info string:
-   --"dbname=d51jr8lvmkfu1g
-   -- host=ec2-54-80-123-146.compute-1.amazonaws.com 
-   --port=5432 user=eiarazeutrjcin 
-   --password=924f432d4bd73a06a569f6ed0f0ffa470b148bab2cf7ef28a414a93d7bca42ac 
-   --sslmode=require"
---Connection URL:
-   --postgres://eiarazeutrjcin:924f432d4bd73a06a569f6ed0f0ffa470b148bab2cf7ef28a414a93d7bca42ac@ec2-54-80-123-146.compute-1.amazonaws.com:5432/d51jr8lvmkfu1g
+
 
 
 
