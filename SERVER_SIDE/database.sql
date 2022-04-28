62c61ad512b33e8f8b5ce1a21c108f1f94b335a5
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-04-29 00:41:28 +0300

    add database file

---
+++ SERVER_SIDE/database.sql
@@ -0,0 +1,31 @@
+-- create table usersInfo
+CREATE TABLE usersInfo (
+    userId SERIAL,
+    fname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phoneNumber INTEGER NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (userId),
+    UNIQUE(email)
+);
+-- create login table
+CREATE TABLE loginInfo (
+    Email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Email)
+);
+
+--create table Trips
+CREATE TABLE Trips(
+    TripId int NOT NULL,
+    userId int NOT NULL,
+    BoatType varchar(255),
+    seats int NOT NULL,
+    prices varchar(255),
+	Routes varchar(255) NOT NULL,
+	primary key(TripId),
+	foreign key(userId) REFERENCES usersInfo(userId)
+);
+----------------------------------------------------------------
+SELECT usersInfo.userId, usersInfo.fName, Trips.BoatType, 
+FROM usersInfo
+INNER JOIN Trips ON usersInfo.userId = Trips.userId;
